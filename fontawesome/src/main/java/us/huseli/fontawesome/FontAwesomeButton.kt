958750eeb0fceaa5dbdd21585b07476d3cc60077
Robert Huselius <robert@huseli.us>
2022-09-12 03:50:39 +0200

    android:enabled on FontAwesomeButton also changes 'clickable'

--- fontawesome/src/main/java/us/huseli/fontawesome/FontAwesomeButton.kt
+++ fontawesome/src/main/java/us/huseli/fontawesome/FontAwesomeButton.kt
@@ -8,15 +8,21 @@ class FontAwesomeButton(context: Context, attrs: AttributeSet?, defStyleAttr: In
     init {
         /** Set ripple effect on click. */
         val arr = intArrayOf(android.R.attr.selectableItemBackgroundBorderless)
-        val ta = context.obtainStyledAttributes(arr)
-        val resId = ta.getResourceId(0, 0)
-        ta.recycle()
-        setBackgroundResource(resId)
+        val a = context.obtainStyledAttributes(arr)
+        for (i in 0 until a.indexCount) {
+            when (val idx = a.getIndex(i)) {
+                0 -> setBackgroundResource(a.getResourceId(idx, 0))
+            }
+        }
+        a.recycle()
     }
 
-    override fun getTextAlignment(): Int {
-        return View.TEXT_ALIGNMENT_CENTER
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        isClickable = enabled
     }
+
+    override fun getTextAlignment() = View.TEXT_ALIGNMENT_CENTER
 
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, R.style.FontAwesomeTextView)
 
